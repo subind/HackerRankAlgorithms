3d70b50e5ba3a6b905305069a7034501d3e8bf1c
Subind <subind214@gmail.com>
2021-03-19 13:05:00 +0530

    Intro tutorial missing code

--- src/main/kotlin/sorting/Intro.kt
+++ src/main/kotlin/sorting/Intro.kt
@@ -1,2 +1,27 @@
 package sorting
 
+import java.util.*
+
+// Complete the introTutorial function below.
+fun introTutorial(V: Int, arr: Array<Int>): Int {
+
+    for(i in arr.indices){
+        if(arr[i] == V){
+            return i
+        }
+    }
+
+    //If not found
+    return -1
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val V = scan.nextLine().trim().toInt()
+    val n = scan.nextLine().trim().toInt()
+    val arr = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+    val result = introTutorial(V, arr)
+    println(result)
+}
+
+
