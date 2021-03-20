f07a7222c0ead8ef7d989b1641c7a9c56de65271
Subind <subind214@gmail.com>
2021-03-20 18:24:57 +0530

    p1

--- src/main/kotlin/sorting/InsertionSortP1.kt
+++ src/main/kotlin/sorting/InsertionSortP1.kt
@@ -1,2 +1,34 @@
 package sorting
 
+import java.util.*
+
+// Complete the insertionSort1 function below.
+fun insertionSort1(n: Int, arr: Array<Int>): Unit {
+
+    var size = arr.size
+
+    val x = arr[size - 1]
+
+    for (i in size - 1 downTo 0) {
+        if(i == 0){
+            arr[i] = x
+            println(arr.joinToString(" "))
+        } else if (arr[i - 1] > x) {
+            arr[i] = arr[i - 1]
+            println(arr.joinToString(" "))
+        } else {
+            arr[i] = x
+            println(arr.joinToString(" "))
+            break
+        }
+    }
+
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val n = scan.nextLine().trim().toInt()
+    val arr = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+    insertionSort1(n, arr)
+}
